589f800b3f10dcae974add0ce812472d11a25a98
woods <han-xuefeng@hotmail.com>
2020-04-08 11:16:35 +0800

    引入路由类调试

--- web/front.php
+++ web/front.php
@@ -6,19 +6,44 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+
+
 $request = Request::createFromGlobals();
 
 $response = new Response();
+
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$routes = new RouteCollection();
+
+$routes->add('hello', new Route('/hello/{name}', array('name' => 'World')));
+$routes->add('hello/ccc', new Route('/hello/ccc'));
+$routes->add('bye', new Route('/bye'));
+
+
+$matcher = new UrlMatcher($routes, $context);
+
+
+$attributes = $matcher->match($request->getPathInfo());
+
+
 
 $map = [
     '/hello'=>__DIR__.'/../src/pages/hello.php',
     '/bye'=>__DIR__.'/../src/pages/bye.php',
 ];
+
 $path = $request->getPathInfo();
 
-if(isset($map[$path])){
+if(isset($map['/'.$attributes['_route']])){
     ob_start();
-    include $map[$path];
+    include $map['/'.$attributes['_route']];//$map[$path];
     $response->setContent(ob_get_clean());
 }else{
     $response->setStatusCode(404);
